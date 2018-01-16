78750bd23f3e8bc4957cf4310717b6006654e4b5
ahsanhabib98 <ahredoan@gmail.com>
2018-01-16 17:21:28 +0600

    added service functionalities

---
+++ services/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ServiceCatagory, ServiceAdd
+
+admin.site.register(ServiceCatagory)
+admin.site.register(ServiceAdd)
+# Register your models here.
