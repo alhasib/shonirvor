afd5e4b52e9d7f1fc1a486e5b3d0321184a78f4e
harunurkst <harun1393@gmail.com>
2018-01-16 18:22:02 +0600

    add all template to root templates directory

--- shonirvor/settings.py
+++ shonirvor/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'shonirvor.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["templates",],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
