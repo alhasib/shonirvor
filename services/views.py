30988e99ca03d454bdd71e05eeb7a1112de1f9a4
rabiulislam993 <rabiulislam993@live.com>
2018-01-17 12:15:26 +0600

    resolve conflicts

--- services/views.py
+++ services/views.py
@@ -5,7 +5,7 @@ from .models import ServiceAdd, ServiceCatagory
 def service_catagory(request):
     form=ServiceCatagory.objects.all()
     context={'form':form}
-    return render(request, 'Service_catagory.html', context)
+    return render(request, 'services/service_catagory.html', context)
 
 def service_add(request):
     if request.method == 'POST':
@@ -18,12 +18,12 @@ def service_add(request):
     else:
         form = ServiceAddForm()
         context = {'form':form}
-        return render(request,'service_add.html', context)
+        return render(request,'services/service_add.html', context)
 
 def service_details(request, provider):
     form = ServiceAdd.objects.get(provider=provider)
     context = {'form':form}
-    return render(request,'service_details.html',context)
+    return render(request,'services/service_details.html',context)
 
 def catagorywise_service(request, service_catagory):
     try:
@@ -31,6 +31,6 @@ def catagorywise_service(request, service_catagory):
         context={'form':form}
     except:
         context={'errmsg':'You have no service'}
-    return render(request,'catagorywise_service.html',context)
+    return render(request,'services/catagorywise_service.html',context)
 
 # Create your views here.
